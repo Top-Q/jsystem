8da91bd1e414c6398fb65a7e90673519c97c2c9f
Nir Armon <nir.armon33@gmail.com>
2015-05-13 10:42:59 +0300

    catch exception

--- jsystem-core-projects/jsystemAnt/src/main/java/com/aqua/anttask/jsystem/JSystemDataDrivenTask.java
+++ jsystem-core-projects/jsystemAnt/src/main/java/com/aqua/anttask/jsystem/JSystemDataDrivenTask.java
@@ -60,7 +60,7 @@ public class JSystemDataDrivenTask extends PropertyReaderTask {
 			// in case file is a reference, if not the file name will be as entered by the user - nir
 			String fileName = (String) ParametersManager.replaceReferenceWithValue(file, ParameterType.FILE);
 			data = provider.provide(new File(file), param);
-		} catch (DataCollectorException e) {
+		} catch (Exception e) {
 			log.log(Level.WARNING, "Failed to collect data due to " + e.getMessage());
 			return;
 		}
