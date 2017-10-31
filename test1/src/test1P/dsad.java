3e239d5e9954f35a260c1ac73024dabb980fa345
Ben-Mark <bm7382598@gmail.com>
2017-10-31 16:50:06 +0200

    JsystemCore backslash fix , caused from the mix of backslashes and properties files
    JSystem Core - FileUtils loadProperties method now intercepts every backslash from both summary and run properties and provides a fix for the dissapearing backslashes bug, JsystemApp fix was deprecated and now handles this issue from within the core

---
+++ test1/src/test1P/dsad.java
@@ -0,0 +1,9 @@
+package test1P;
+
+public class dsad {
+	public static void main(String[] args) {
+		String n = "com.aqua.services.ParameterProvider.SampleBean;#Tue Oct 31 10:41:24 IST 2017\r\nBeanName=ben\\\\\\\\\\\\\\\\mark\r\nFile=${summary\\\\\\:testPath}\r\nId=0\r\n";
+		StringBuilder sb = new StringBuilder(n);
+		String s = sb.toString();
+	}
+}
