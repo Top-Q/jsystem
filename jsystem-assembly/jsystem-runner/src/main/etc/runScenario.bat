137f68f467c54463d3416d993835b7438f9b96b2
Kobi Cohen <Kobi_Cohen@contractor.amat.com>
2018-02-14 16:13:48 +0200

    Java 9 Support - Enable JSystem to run with Java 9 #319

--- jsystem-assembly/jsystem-runner/src/main/etc/runScenario.bat
+++ jsystem-assembly/jsystem-runner/src/main/etc/runScenario.bat
@@ -20,9 +20,15 @@ echo # > .testPropertiesFile_Empty
 
 del .run.properties
 
+::add modules(xml.bind/java.desktop) in case running on java 9+ version
+set ADD_MODULES_STR=
+for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -fullversion 2^>^&1') do set "jver=%%j"
+if %jver% NEQ 1 set ADD_MODULES_STR=--add-modules java.xml.bind --add-modules java.desktop
+
+
 set ANT_HOME=thirdparty\ant
 set ANT_CMD=%ANT_HOME%\bin\ant.bat
-set ANT_OPTS=-Djsystem.current.scenario.name=%SCENARIO_NAME% -Dbasedir=. -Dscenarios.base=%PROJECT_CLASSES_PATH% -DsutFile=%SUT_FILE% -Xms32M -Xmx256M
+set ANT_OPTS=%ADD_MODULES_STR% -Djsystem.current.scenario.name=%SCENARIO_NAME% -Dbasedir=. -Dscenarios.base=%PROJECT_CLASSES_PATH% -DsutFile=%SUT_FILE% -Xms32M -Xmx256M
 rem set ANT_OPTS=%ANT_OPTS% -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y
 "%ANT_CMD%" -lib thirdparty\ant\lib -lib thirdparty\commonLib -lib thirdparty\lib -lib lib -lib customer_lib -lib %PROJECT_CLASSES_PATH%\..\..\lib -lib %PROJECT_CLASSES_PATH%\..\lib -lib %PROJECT_CLASSES_PATH% -listener jsystem.runner.AntExecutionListener -f %PROJECT_CLASSES_PATH%\%SCENARIO_NAME%.xml
 
