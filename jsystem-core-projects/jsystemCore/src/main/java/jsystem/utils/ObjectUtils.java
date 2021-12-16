04c5f7a1e1b541241a15b2daffc4cf0500262087
itaiag <itai.agmon@gmail.com>
2021-12-16 14:49:18 +0200

    Removed Spring dependency since it was using log4j. issue #360

---
+++ jsystem-core-projects/jsystemCore/src/main/java/jsystem/utils/ObjectUtils.java
@@ -0,0 +1,63 @@
+package jsystem.utils;
+
+import java.util.Arrays;
+
+public class ObjectUtils {
+
+
+    /**
+     * Determine if the given objects are equal, returning <code>true</code>
+     * if both are <code>null</code> or <code>false</code> if only one is
+     * <code>null</code>.
+     * <p>Compares arrays with <code>Arrays.equals</code>, performing an equality
+     * check based on the array elements rather than the array reference.
+     *
+     * ITAI: This function was copied from spring since we needed to take it out as part
+     * of handling the log4j security vulnerabilities. Issue #360
+     *
+     *
+     * @param o1 first Object to compare
+     * @param o2 second Object to compare
+     * @return whether the given objects are equal
+     * @see java.util.Arrays#equals
+     */
+    public static boolean nullSafeEquals(Object o1, Object o2) {
+        if (o1 == o2) {
+            return true;
+        }
+        if (o1 == null || o2 == null) {
+            return false;
+        }
+        if (o1.equals(o2)) {
+            return true;
+        }
+        if (o1 instanceof Object[] && o2 instanceof Object[]) {
+            return Arrays.equals((Object[]) o1, (Object[]) o2);
+        }
+        if (o1 instanceof boolean[] && o2 instanceof boolean[]) {
+            return Arrays.equals((boolean[]) o1, (boolean[]) o2);
+        }
+        if (o1 instanceof byte[] && o2 instanceof byte[]) {
+            return Arrays.equals((byte[]) o1, (byte[]) o2);
+        }
+        if (o1 instanceof char[] && o2 instanceof char[]) {
+            return Arrays.equals((char[]) o1, (char[]) o2);
+        }
+        if (o1 instanceof double[] && o2 instanceof double[]) {
+            return Arrays.equals((double[]) o1, (double[]) o2);
+        }
+        if (o1 instanceof float[] && o2 instanceof float[]) {
+            return Arrays.equals((float[]) o1, (float[]) o2);
+        }
+        if (o1 instanceof int[] && o2 instanceof int[]) {
+            return Arrays.equals((int[]) o1, (int[]) o2);
+        }
+        if (o1 instanceof long[] && o2 instanceof long[]) {
+            return Arrays.equals((long[]) o1, (long[]) o2);
+        }
+        if (o1 instanceof short[] && o2 instanceof short[]) {
+            return Arrays.equals((short[]) o1, (short[]) o2);
+        }
+        return false;
+    }
+}
